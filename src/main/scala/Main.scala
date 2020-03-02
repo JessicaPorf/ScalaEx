07b91cac58fec6a0a8a1cd600632746786afe5a7
JessicaPorf <trylegolemon@gmail.com>
2020-03-02 16:46:31 +0000

    added HelloWold, Assignment,Parameters

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,23 @@
 object Main extends App {
-
+  //Basic Exercise Book
+  //Hello World
   println("Hello world!")
 
+
+  //Assignment
+  var Hello = "Hello World!"
+
+
+    {
+  println(Hello)
 }
+  //Parameters
+  val greeting: String = "Hello, world!"
+
+
+    println( greeting )
+
+  // Return Types
+  //Type Interface
+}
+
