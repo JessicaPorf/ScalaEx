abc2d74a2e49fdc8ba47ea09a3171981fdb77018
JessicaPorf <trylegolemon@gmail.com>
2020-03-02 17:26:23 +0000

    added HelloWold, Assignment,Parameters

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -19,5 +19,20 @@ object Main extends App {
 
   // Return Types
   //Type Interface
-}
+
+//  if int a < int b || int c
+//  return int b * int c
+
+// Multiply two numbers End of Day Question
+
+  def max(a:Int, b:Int, c:Int) : Int = if (a > b ) a else if
+
+//Beer End of Day Question
+    for(counter <- 100 to 1 by -1)
+      print(counter + " bottles of beer on the wall \n " );
+
+    println();
+  }
+
+
 
