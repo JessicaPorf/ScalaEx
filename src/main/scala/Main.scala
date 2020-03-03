830acd6f03672f0ba8ce423bcb56502aa876f0f7
JessicaPorf <trylegolemon@gmail.com>
2020-03-03 09:16:19 +0000

    added Type Interface

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -17,7 +17,34 @@ object Main extends App {
 
     println( greeting )
 
-  // Return Types
+  // Type interface
+
+  var b: Boolean = true
+  var b1: Byte = 126
+  var b2: Float = 2.45673f
+  var b3: Int = 3
+  var b4: Short = 45
+  var b5: Double = 2.93846523
+  var b6: Char = 'A'
+  if (b == true)
+  {
+    println("boolean:qa consulting")
+  }
+  println("byte:" + b1)
+  println("float:" + b2)
+  println("integer:" + b3)
+  println("short:" + b4)
+  println("double:" + b5)
+  println("char:" + b6)
+}
+
+
+//Strings
+
+
+
+
+// Return Types
   //Type Interface
 
 //  if int a < int b || int c
